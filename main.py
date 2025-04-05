f0ab92070511d2cdf6a5f8c86bad410baae04ee5
Malick Ould Hamdi <109855300+Ouldgeneral@users.noreply.github.com>
2025-04-05 15:00:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,20 @@
 from queue import LifoQueue as stack
 from tkinter import*
 from tkinter import filedialog
+from tkinter import messagebox as box
 undo_stack=stack()
 redo_stack=stack()
-empty_stack=stack()
 file=''
 file_container=''
-file_types={'txt':'PleinText','html':'HTML','htm','HTML','js':'JavaScript','java':'Java','cpp':'C++',
+file_types={'txt':'PleinText','html':'HTML' ,'htm':'HTML','js':'JavaScript','java':'Java','cpp':'C++',
             'c':'C','asm':'Assembly','css':'CSS','cs':'C#','php':'PHP','py':'Python','sh':'Shell'}
 def filetype():
-  file=file.split('/')
-  file=file[-1]
-  file=file.split('.')
-  window.title(f'{file[0]}-Recipe')
+  file_name=file.split('/')
+  file_name=file_name[-1]
+  file_name=file_name.split('.')
+  window.title(f'{file_name[0]}-Recipe')
   try:
-    type['text']=file_types[file[-1]]
+    type['text']=file_types[file_name[-1]]
     type['fg']='green'
   except KeyError:
     type['text']='Unknown'
@@ -26,52 +26,59 @@ def save_file_as():
   save_file()
 def clear_stack():
     global undo_stack,redo_stack
-    redo_stack=empty_stack
-    undo_stack=empty_stack
+    redo_stack=stack()
+    undo_stack=stack()
 def create_new_file():
   clear_stack()
   global file
   file=filedialog.asksaveasfilename(title="Enter new file name",filetypes=[('*','*')])
   show_file()
 def save_file():
-  with open(file,'w') as the_file:
-    the_file.write(file_container.get('1.0',END))
+  try:
+      with open(file,'w') as the_file:
+        the_file.write(file_container.get('1.0',END))
+  except PermissionError:
+      box.showwarning('Permission Denied','Write Failure ')
 def show_file():
   filetype()
   file_container.delete('1.0',END)
   try:
     text=open(file,"r")
     file_container.insert(INSERT,text.read())
+    undo_stack.put(text.read())
     text.close()
   except FileNotFoundError:file_container.insert(INSERT,'')
-  except AttributeError:file_container.insert(INSERT,file)
 def read_file():
   clear_stack()
   global file
-  file=filedialog.askopenfilename(title="Open file",filetypes=[('*','*')]
+  file=filedialog.askopenfilename(title="Open file",filetypes=[('*','*')])
   show_file()
 def line_count():
-  lines=sum(1 for line in file_container.get('1.0',END))
+  lines=len(file_container.get('1.0',END).splitlines())
   lines=str(lines)
   line['text']=lines
 def undo():
   global undo_stack,redo_stack
   if undo_stack.empty():return
+  current=file_container.get('1.0',END)
   text=undo_stack.get()
-  swap(text)
-  redo_stack.put(text)
-def swap(x):
-  global file
-  y=file
-  file=x
-  show_file()
-  file=y
+  update(text)
+  redo_stack.put(current)
 def redo():
   global undo_stack,redo_stack
   if redo_stack.empty():return
+  current=file_container.get('1.0',END)
   text=redo_stack.get()
-  swap(text)
-  undo_stack.put(text)
+  update(text)
+  undo_stack.put(current)
+def update(text):
+  file_container.delete('1.0',END)
+  file_container.insert(INSERT,text)
+def on_return(event):
+  line_count()
+  txt=file_container.get('1.0',END)
+  undo_stack.put(txt)
+  
 #Program main entry
 window=Tk()
 window.title("Recipes Pad Pro")
@@ -88,12 +95,11 @@ line.pack(side=LEFT)
 type=Label(buttons_area,text='')
 type.pack(side=LEFT)
 line_count()
-window.bind('<Return>',lambda event:line_count())
+window.bind('<Return>',lambda event:on_return(event))
 window.bind('<Control-s>', lambda event:save_file())
 window.bind('<Control-o>', lambda event:read_file())
-window.bind('<Return>', lambda event:undo_stack.put(file_container.get('1.0',END)))
 window.bind('<Control-z>',lambda event:undo())
 window.bind('<Control-y>',lambda event:redo())
-window.bind('<Control-n>',lambda event:create-new_file())
+window.bind('<Control-n>',lambda event:create_new_file())
 window.mainloop()
     
