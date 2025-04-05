0160cdc9fe9047dcc348ac14c55e8eca37ade1b2
Malick Ould Hamdi <109855300+Ouldgeneral@users.noreply.github.com>
2025-04-05 16:07:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,23 +3,56 @@ from queue import LifoQueue as stack
 from tkinter import*
 from tkinter import filedialog
 from tkinter import messagebox as box
+import os
+import subprocess
 undo_stack=stack()
 redo_stack=stack()
 file=''
+file_name =''
 file_container=''
+path=''
 file_types={'txt':'PleinText','html':'HTML' ,'htm':'HTML','js':'JavaScript','java':'Java','cpp':'C++',
             'c':'C','asm':'Assembly','css':'CSS','cs':'C#','php':'PHP','py':'Python','sh':'Shell'}
+def run():
+  compilers={
+              'java':f'javac {file}',
+              'c':f'gcc {file} -o {file_name}.out',
+              'cpp':f'g++ {file} -o {file_name}.out',
+              'cs':f'csc {file}'}
+  runners={
+              'java':f'java {path}{file_name}',
+              'c':f'./{path}{file_name}.out',
+              'cs':f'start {path}{file_name}'}
+  extension=filetype().lower()
+  if extension=='py':return subprocess.run(['python',file])
+  elif extension =='html' or extension =='htm':return os.system(['start',file])
+  elif extension =='js':return os.system(['node' file])
+  elif extension=='sh':return os.system(['bash' ,file])
+  else:
+    try:
+         os.system(compilers[extension])
+         if extension=='c' or extension =='cpp':return subprocess.run(runners['c'])
+         return subprocess.run(runners[extension])
+    except KeyError:box.showwarning('Compilation  Error','Unsupported compiler')
+    except subprocess.CalledProcessError as e:
+         box.showwarning('Error',f'Error:\n{e}')
 def filetype():
+  global file_name,path
   file_name=file.split('/')
+  path=file_name[:-1]
+  path='/'.join(path)+'/'
   file_name=file_name[-1]
   file_name=file_name.split('.')
   window.title(f'{file_name[0]}-Recipe')
+  file_name=file_name[0]
   try:
     type['text']=file_types[file_name[-1]]
     type['fg']='green'
+    return file_name[-1]
   except KeyError:
     type['text']='Unknown'
     type['fg']='red'
+    return ''
 def save_file_as():
   global file
   file=filedialog.asksaveasfilename(title="Enter file name",filetypes=[('*','*')])
@@ -43,10 +76,9 @@ def show_file():
   filetype()
   file_container.delete('1.0',END)
   try:
-    text=open(file,"r")
-    file_container.insert(INSERT,text.read())
-    undo_stack.put(text.read())
-    text.close()
+    with open(file,'r') as text:
+      file_container.insert(INSERT,text.read())
+      undo_stack.put(text.read())
   except FileNotFoundError:file_container.insert(INSERT,'')
 def read_file():
   clear_stack()
@@ -86,7 +118,7 @@ file_container=Text(window)
 file_container.pack(side=TOP,fill=BOTH,expand=YES)
 buttons_area=Frame(window)
 buttons_area.pack(side=TOP,fill=BOTH,expand=YES)
-my_buttons={'Save':save_file,'Read':read_file,'Save as':save_file_as,'New file':create_new_file,'Quit':window.quit}
+my_buttons={'Run':run,'Save':save_file,'Read':read_file,'Save as':save_file_as,'New file':create_new_file,'Quit':window.quit}
 for button in my_buttons:
   button=Button(buttons_area,text=button,command=lambda i=my_buttons[button]:i())
   button.pack(side=LEFT,fill=BOTH,expand=YES)
@@ -101,5 +133,6 @@ window.bind('<Control-o>', lambda event:read_file())
 window.bind('<Control-z>',lambda event:undo())
 window.bind('<Control-y>',lambda event:redo())
 window.bind('<Control-n>',lambda event:create_new_file())
+window.bind('<F5>',lambda event: run())
 window.mainloop()
     
